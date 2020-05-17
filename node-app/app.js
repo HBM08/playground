5aa9f8cccb2a16a70ee5e87b1ce76122b97dc7c6
HBM08 <bogdan.hotea@ymail.com>
2020-05-18 00:03:01 +0300

    Create db connection

--- node-app/app.js
+++ node-app/app.js
@@ -2,6 +2,32 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// TODO: configure express below 
-// TODO: configure DB connection
+const Sequelize = require('sequelize');
+const DISABLE_SEQUELIZE_DEFAULTS = {
+  timestamps: false,
+  freezeTableName: true,
+};
+
+const { DataTypes } = Sequelize;
+const sequelize = new Sequelize({
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  host: process.env.DEV_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.POSTGRES_PASSWORD,
+  dialect: process.env.DB_TYPE
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('DB connection has been established successfully.');
+    app.listen(PORT, () => {
+      console.log(`Listening on PORT ${PORT}\n`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
